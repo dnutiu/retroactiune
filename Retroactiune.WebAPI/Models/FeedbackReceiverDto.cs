9489ec6f33982493db0e801e71d59af6a90a24fc
Denis Nutiu <denis.nutiu@gmail.com>
2021-05-29 21:24:41 +0300

    Modify FeedbackReceiverDto, mark fields with Required

--- Retroactiune.WebAPI/Models/FeedbackReceiverDto.cs
+++ Retroactiune.WebAPI/Models/FeedbackReceiverDto.cs
@@ -1,12 +1,16 @@
-﻿namespace Retroactiune.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Retroactiune.Models
 {
     /// <summary>
     /// FeedbackReceiverDto is the DTO for <see cref="FeedbackReceiver"/>
     /// </summary>
     public class FeedbackReceiverDto
     {
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Description { get; set; }
     }
 }
