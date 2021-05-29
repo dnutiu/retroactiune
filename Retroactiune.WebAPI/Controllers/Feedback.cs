ebd4bd8ac345b9e2b6043c8db9193976fb4c8399
Denis Nutiu <denis.nutiu@gmail.com>
2021-05-29 20:01:15 +0300

    Add Tokens.cs and Feedback.cs

---
+++ Retroactiune.WebAPI/Controllers/Feedback.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Retroactiune.Controllers
+{
+    public class Feedback : ControllerBase
+    {
+        // TODO Add feedback, list feedback, delete feedback, ged feedback.
+    }
+}
---
+++ Retroactiune.WebAPI/Controllers/Tokens.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Retroactiune.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class Tokens : ControllerBase
+    {
+        // TODO: Add tokens, list tokens (unused, used), delete tokens.
+    }
+}
