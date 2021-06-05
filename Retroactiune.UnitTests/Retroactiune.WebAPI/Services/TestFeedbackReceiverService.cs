2eb29a4a2575b0e1321094e492eb4be9aca77921
Denis Nutiu <denis.nutiu@gmail.com>
2021-06-05 15:34:04 +0300

    Add DELETE unit test for FeedbackReceiverService

--- Retroactiune.UnitTests/Retroactiune.WebAPI/Services/TestFeedbackReceiverService.cs
+++ Retroactiune.UnitTests/Retroactiune.WebAPI/Services/TestFeedbackReceiverService.cs
@@ -78,13 +78,16 @@ namespace Retroactiune.Tests.Retroactiune.WebAPI.Services
             mongoClientMock
                 .Verify(i => i.GetDatabase("MyDB", null), Times.Once());
             mongoDatabaseMock
-                .Verify(i => i.GetCollection<FeedbackReceiver>("feedback_receiver", It.IsAny<MongoCollectionSettings>()),
-                Times.Once());
+                .Verify(
+                    i => i.GetCollection<FeedbackReceiver>("feedback_receiver", It.IsAny<MongoCollectionSettings>()),
+                    Times.Once());
             mongoCollectionMock
-                .Verify(i => i.InsertManyAsync(It.IsAny<IEnumerable<FeedbackReceiver>>(), It.IsAny<InsertManyOptions>(), It.IsAny<CancellationToken>()),
-                Times.Never());
+                .Verify(
+                    i => i.InsertManyAsync(It.IsAny<IEnumerable<FeedbackReceiver>>(), It.IsAny<InsertManyOptions>(),
+                        It.IsAny<CancellationToken>()),
+                    Times.Never());
         }
-        
+
         [Fact]
         public void Test_CreateManyAsync_NoItems()
         {
@@ -116,11 +119,49 @@ namespace Retroactiune.Tests.Retroactiune.WebAPI.Services
             mongoClientMock
                 .Verify(i => i.GetDatabase("MyDB", null), Times.Once());
             mongoDatabaseMock
-                .Verify(i => i.GetCollection<FeedbackReceiver>("feedback_receiver", It.IsAny<MongoCollectionSettings>()),
+                .Verify(
+                    i => i.GetCollection<FeedbackReceiver>("feedback_receiver", It.IsAny<MongoCollectionSettings>()),
                     Times.Once());
             mongoCollectionMock
-                .Verify(i => i.InsertManyAsync(It.IsAny<IEnumerable<FeedbackReceiver>>(), It.IsAny<InsertManyOptions>(), It.IsAny<CancellationToken>()),
+                .Verify(
+                    i => i.InsertManyAsync(It.IsAny<IEnumerable<FeedbackReceiver>>(), It.IsAny<InsertManyOptions>(),
+                        It.IsAny<CancellationToken>()),
                     Times.Never());
+        }
+
+        [Fact]
+        public async Task Test_Delete_Ok()
+        {
+            // Arrange
+            var mongoDatabaseMock = new Mock<IMongoDatabase>();
+            var mongoClientMock = new Mock<IMongoClient>();
+            var mongoSettingsMock = new Mock<IMongoDbSettings>();
+            var mongoCollectionMock = new Mock<IMongoCollection<FeedbackReceiver>>();
+
+            mongoSettingsMock.SetupGet(i => i.DatabaseName).Returns("MyDB");
+            mongoSettingsMock.SetupGet(i => i.FeedbackReceiverCollectionName).Returns("feedback_receiver");
+
+            mongoClientMock
+                .Setup(stub => stub.GetDatabase(It.IsAny<string>(), It.IsAny<MongoDatabaseSettings>()))
+                .Returns(mongoDatabaseMock.Object);
+
+            mongoDatabaseMock
+                .Setup(i => i.GetCollection<FeedbackReceiver>(It.IsAny<string>(), It.IsAny<MongoCollectionSettings>()))
+                .Returns(mongoCollectionMock.Object);
+
+            // Test
+            var service = new FeedbackReceiverService(mongoClientMock.Object, mongoSettingsMock.Object);
+            await service.DeleteOneAsync("insert_guid_here");
+
+            // Assert
+            mongoClientMock.Verify(i => i.GetDatabase("MyDB", null), Times.Once());
+            mongoDatabaseMock.Verify(
+                i => i.GetCollection<FeedbackReceiver>("feedback_receiver", It.IsAny<MongoCollectionSettings>()),
+                Times.Once());
+
+            mongoCollectionMock.Verify(
+                i => i.DeleteOneAsync(It.IsAny<FilterDefinition<FeedbackReceiver>>(), It.IsAny<CancellationToken>()),
+                Times.Once());
         }
     }
 }
