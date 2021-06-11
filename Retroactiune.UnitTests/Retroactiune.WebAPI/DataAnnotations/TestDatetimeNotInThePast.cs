e8166dc5f83ff5907f8a44b048def5edbb6895da
Denis Nutiu <denis.nutiu@gmail.com>
2021-06-11 22:55:34 +0300

    Add unit tests for DatetimeNotInThePast annotation.

---
+++ Retroactiune.UnitTests/Retroactiune.WebAPI/DataAnnotations/TestDatetimeNotInThePast.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Retroactiune.DataAnnotations;
+using Xunit;
+
+namespace Retroactiune.Tests.Retroactiune.WebAPI.DataAnnotations
+{
+    public class TestDatetimeNotInThePast
+    {
+        [Fact]
+        public void Test_DatetimeNotInThePast_NullDate()
+        {
+            var test = new DatetimeNotInThePast();
+            Assert.True(test.IsValid(null));
+        }
+        
+        [Fact]
+        public void Test_DatetimeNotInThePast_FutureDate()
+        {
+            var test = new DatetimeNotInThePast();
+            var futureDate = DateTime.UtcNow.AddDays(1);
+            Assert.True(test.IsValid(futureDate));
+        }
+        
+        [Fact]
+        public void Test_DatetimeNotInThePast_PastDate()
+        {
+            var test = new DatetimeNotInThePast();
+            var pastDate = DateTime.UtcNow.Subtract(new TimeSpan(1));
+            Assert.False(test.IsValid(pastDate));
+        }
+    }
+}
