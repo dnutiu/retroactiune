901d495bff505c04f998502155dc5b67fb18c595
Denis Nutiu <denis.nutiu@gmail.com>
2021-07-04 15:24:57 +0300

    Implement Feedback.cs model.

--- Retroactiune.Core/Entities/Feedback.cs
+++ Retroactiune.Core/Entities/Feedback.cs
@@ -1,7 +1,40 @@
-﻿namespace Retroactiune.Core.Entities
+﻿using System;
+using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Retroactiune.Core.Entities
 {
+    /// <summary>
+    /// Feedback is the feedback given to the <see cref="FeedbackReceiver"/>.
+    /// </summary>
     public class Feedback
     {
-        // TODO: Entity model for feedback.
+        private uint _rating;
+
+        [BsonId, JsonPropertyName("id")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [JsonPropertyName("rating")]
+        public uint Rating
+        {
+            get => _rating;
+            set
+            {
+                if (value <= 5)
+                {
+                    _rating = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        [JsonPropertyName("description")] public string Description { get; set; }
+
+        [JsonPropertyName("created_at")] public DateTime CreatedAt { get; set; }
     }
 }
